1fcf8b342d19b71ddaf7d3cdb5661c29ac145237
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-08-27 20:42:45 -0300

    Create goaccess_now.sh

---
+++ goaccess_now.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SITE=$1
+
+mkdir /tmp/goaccess
+mkdir /tmp/goaccess/$SITE
+
+scp your_user@your_host:/var/log/nginx/$(echo $SITE)_access.log /tmp/goaccess/$SITE
+
+echo $SITE
+
+docker run --rm -v /tmp/goaccess/$(echo $SITE):/tmp/goaccess -v /home/usr_amcom/goaccess:/tmp/html -v /home/usr_amcom/goaccess_db/$(echo $SITE):/tmp/goaccess_db allinurl/goaccess -f /tmp/goaccess/$(echo $SITE)_access.log -o /tmp/html/$(echo $SITE).html --log-format=COMBINED --persist --restore --db-path /tmp/goaccess_db/ --keep-last=1800
