25e88f47fb3d22194a60a703701db7370532ec23
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-08-27 20:48:18 -0300

    Update goaccess_now.sh

--- goaccess_now.sh
+++ goaccess_now.sh
@@ -3,10 +3,12 @@ SITE=$1
 
 mkdir /tmp/goaccess
 mkdir /tmp/goaccess/$SITE
+mkdir $home/goaccess
+mkdir $home/goaccess_db
 
 #You have to be access with SSH key... Check the path of your nginxs logs
 scp your_user@your_remote_host:/var/log/nginx/$(echo $SITE)_access.log /tmp/goaccess/$SITE
 
 echo $SITE
 
-docker run --rm -v /tmp/goaccess/$(echo $SITE):/tmp/goaccess -v /home/usr_amcom/goaccess:/tmp/html -v /home/usr_amcom/goaccess_db/$(echo $SITE):/tmp/goaccess_db allinurl/goaccess -f /tmp/goaccess/$(echo $SITE)_access.log -o /tmp/html/$(echo $SITE).html --log-format=COMBINED --persist --restore --db-path /tmp/goaccess_db/ --keep-last=1800
+docker run --rm -v /tmp/goaccess/$(echo $SITE):/tmp/goaccess -v $home/goaccess:/tmp/html -v $home/goaccess_db/$(echo $SITE):/tmp/goaccess_db allinurl/goaccess -f /tmp/goaccess/$(echo $SITE)_access.log -o /tmp/html/$(echo $SITE).html --log-format=COMBINED --persist --restore --db-path /tmp/goaccess_db/ --keep-last=1800
